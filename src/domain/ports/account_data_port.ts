d7303cf731327c6850dc9ae645370745d6fe8d01
Thiago Guerrero <tgbalera@hotmail.com>
2022-07-16 19:32:08 -0300

    fix typo
    Signed-off-by: Thiago Guerrero <tgbalera@hotmail.com>

--- src/domain/ports/account_data_port.ts
+++ src/domain/ports/account_data_port.ts
@@ -7,7 +7,7 @@ export interface AccountQuery {
 export default interface IAccountDataPort {
   create: (account: Account, profileId: string) => Promise<string>;
   get: (query: AccountQuery) => Promise<Account[]>;
-  isLoggedIn: (sccountId: string) => Promise<boolean>;
+  isLoggedIn: (accountId: string) => Promise<boolean>;
   logIn: (accountId: string) => Promise<string>;
   deleteById: (id: string) => Promise<boolean>;
 }
