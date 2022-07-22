5e65dceece1b8f18d252ba86e568ed7f4cedfc32
Vinicius Frota <viniciuspxfrota@gmail.com>
2022-07-21 23:04:59 -0300

    finish implementation without testin

--- src/app/express/controllers/chat_controller.ts
+++ src/app/express/controllers/chat_controller.ts
@@ -22,25 +22,30 @@ export default class ChatController {
   }
 
   private async createChat(req: Request, resp: Response) {
+    const session: string = req.body.session;
+    delete req.body.session;
     const input: ICreateChat = req.body as ICreateChat;
 
-    const id = await this.chatUsecases.createChat(input);
+    const id = await this.chatUsecases.createChat(session, input);
     if (id != null) resp.status(201).json(id);
     else resp.sendStatus(400);
   }
 
   private async sendMessage(req: Request, resp: Response) {
     const chatId = req.params.id;
+    const session: string = req.body.session;
+    delete req.body.session;
     const input: ISendMessage = req.body as ISendMessage;
 
-    if (await this.chatUsecases.sendMessage(chatId, input))
+    if (await this.chatUsecases.sendMessage(session, chatId, input))
       resp.sendStatus(201);
     else resp.sendStatus(400);
   }
 
   private async getChatById(req: Request, resp: Response) {
     const chatID = req.params.id;
-    const chat = await this.chatUsecases.getChatById(chatID);
+    const session: string = req.body.session;
+    const chat = await this.chatUsecases.getChatById(session, chatID);
 
     if (chat) resp.status(200).json(chat);
     else resp.sendStatus(400);
