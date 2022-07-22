e4ba6f152bdec17fc323ac97ac0dc11735005159
Vinicius Frota <viniciuspxfrota@gmail.com>
2022-07-21 23:04:59 -0300

    finish implementation without testin

--- src/app/express/controllers/chat_group_controller.ts
+++ src/app/express/controllers/chat_group_controller.ts
@@ -25,34 +25,42 @@ export default class ChatGroupController {
   }
 
   private async createChat(req: Request, resp: Response) {
+    const session: string = req.body.session;
+    delete req.body.session;
     const input: ICreateGroupChat = req.body as ICreateGroupChat;
 
-    const id = await this.chatGroupUsecases.createChat(input);
+    const id = await this.chatGroupUsecases.createChat(session, input);
     if (id != null) resp.status(201).json(id);
     else resp.sendStatus(400);
   }
 
   private async sendMessage(req: Request, resp: Response) {
+    const session: string = req.body.session;
+    delete req.body.session;
     const chatId = req.params.id;
     const input: ISendMessage = req.body as ISendMessage;
 
-    if (await this.chatGroupUsecases.sendMessage(chatId, input))
+    if (await this.chatGroupUsecases.sendMessage(session, chatId, input))
       resp.sendStatus(201);
     else resp.sendStatus(400);
   }
 
   private async addUser(req: Request, resp: Response) {
+    const session: string = req.body.session;
+    delete req.body.session;
     const chatId = req.params.id;
     const input: IAddUser = req.body as IAddUser;
 
-    if (await this.chatGroupUsecases.addUser(chatId, input))
+    if (await this.chatGroupUsecases.addUser(session, chatId, input))
       resp.sendStatus(201);
     else resp.sendStatus(400);
   }
 
   private async getChatById(req: Request, resp: Response) {
+    const session: string = req.body.session;
+    delete req.body.session;
     const chatID = req.params.id;
-    const chat = await this.chatGroupUsecases.getChatById(chatID);
+    const chat = await this.chatGroupUsecases.getChatById(session, chatID);
 
     if (chat) resp.status(200).json(chat);
     else resp.sendStatus(400);
