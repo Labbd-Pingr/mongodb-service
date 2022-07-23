72925660749ae97594c511773ba79e5edba3bc6d
Vinicius Frota <viniciuspxfrota@gmail.com>
2022-07-22 21:59:22 -0300

    finish chat ???

--- src/domain/usecases/chat.ts
+++ src/domain/usecases/chat.ts
@@ -3,20 +3,43 @@ import IChatDataPort from '../ports/chat_data_port';
 import { v4 } from 'uuid';
 import { ICreateChat, ISendMessage } from './interfaces/interface.chat';
 import AutheticationUsecases from './auth';
+import { UsecaseResponse } from './interfaces/interface';
 
 export default class ChatUsecases {
   constructor(protected readonly chatDataPort: IChatDataPort) {}
 
   @AutheticationUsecases.authorize()
-  public async createChat(sessionId: string, { accountIds }: ICreateChat) {
+  public async createChat(
+    sessionId: string,
+    { accountIds }: ICreateChat
+  ): Promise<UsecaseResponse<string>> {
+    const accountId = await (
+      await AutheticationUsecases.sessionUsecases.getAndValidateSession(
+        sessionId
+      )
+    ).response;
+
+    if (accountId && !accountIds.includes(accountId)) {
+      console.log('[ERROR] The accountIds do not include the user');
+      return {
+        succeed: false,
+        errors: 'The accountIds do not include the user',
+      };
+    }
     const id = v4();
     const chat = new Chat(id, accountIds);
     const dbId = await this.chatDataPort.saveChat(chat);
     if (dbId == undefined) {
       console.log('[ERROR] Chat was not able to be created!');
-      return null;
+      return {
+        succeed: false,
+        errors: 'Chat was not able to be created!',
+      };
     }
-    return id;
+    return {
+      succeed: true,
+      response: id,
+    };
   }
 
   @AutheticationUsecases.authorize()
@@ -24,8 +47,20 @@ export default class ChatUsecases {
     sessionId: string,
     chatId: string,
     { senderId, text }: ISendMessage
-  ): Promise<boolean> {
+  ): Promise<UsecaseResponse<boolean>> {
     try {
+      const accountId = await (
+        await AutheticationUsecases.sessionUsecases.getAndValidateSession(
+          sessionId
+        )
+      ).response;
+
+      if (accountId != senderId) {
+        throw new Error(
+          'Sender ID is different from obtained by account session'
+        );
+      }
+
       const chats = await this.chatDataPort.get({ id: chatId });
       if (chats.length == 0) throw new Error('Chat does not exist!');
 
@@ -34,11 +69,17 @@ export default class ChatUsecases {
 
       const dbId = await this.chatDataPort.addChatMessage(chat, message);
       if (dbId == undefined) throw new Error();
-      return true;
+      return {
+        succeed: true,
+        response: true,
+      };
     } catch (e) {
       const error: Error = e as Error;
       console.log(`[ERROR] Message was not able to be sent! ${error.message}`);
-      return false;
+      return {
+        succeed: false,
+        errors: `Message was not able to be sent! ${error.message}`,
+      };
     }
   }
 
@@ -46,21 +87,42 @@ export default class ChatUsecases {
   public async getChatByAccountIds(
     sessionId: string,
     accountIds: string[]
-  ): Promise<Chat | null> {
+  ): Promise<UsecaseResponse<Chat>> {
     try {
+      const accountId = await (
+        await AutheticationUsecases.sessionUsecases.getAndValidateSession(
+          sessionId
+        )
+      ).response;
+
+      if (accountId && !accountIds.includes(accountId)) {
+        console.log('[ERROR] The accountIds do not include the user');
+        return {
+          succeed: false,
+          errors: 'The accountIds do not include the user',
+        };
+      }
+
       accountIds.sort();
       const chats = await this.chatDataPort.get({ accountIds: accountIds });
 
       if (chats.length == 0) throw new Error('Chat does not exist!');
 
       const chat = chats[0];
-      return chat;
+
+      return {
+        succeed: true,
+        response: chat,
+      };
     } catch (e) {
       const error: Error = e as Error;
       console.log(
         `[ERROR] Chat was not able to be retrieved! ${error.message}`
       );
-      return null;
+      return {
+        succeed: false,
+        errors: `[ERROR] Chat was not able to be retrieved! ${error.message}`,
+      };
     }
   }
 
@@ -68,20 +130,37 @@ export default class ChatUsecases {
   public async getChatById(
     sessionId: string,
     chatId: string
-  ): Promise<Chat | null> {
+  ): Promise<UsecaseResponse<Chat>> {
     try {
       const chats = await this.chatDataPort.get({ id: chatId });
       console.log(chatId, chats, 'aaaaaaah');
       if (chats.length == 0) throw new Error('Chat does not exist!');
 
+      const accountId = await (
+        await AutheticationUsecases.sessionUsecases.getAndValidateSession(
+          sessionId
+        )
+      ).response;
+
       const chat = chats[0];
-      return chat;
+      if (accountId && !chat.accountIds.includes(accountId)) {
+        throw new Error(
+          'Sender ID is different from obtained by account session'
+        );
+      }
+      return {
+        succeed: true,
+        response: chat,
+      };
     } catch (e) {
       const error: Error = e as Error;
       console.log(
         `[ERROR] Chat was not able to be retrieved! ${error.message}`
       );
-      return null;
+      return {
+        succeed: false,
+        errors: `[ERROR] Chat was not able to be retrieved! ${error.message}`,
+      };
     }
   }
 }
